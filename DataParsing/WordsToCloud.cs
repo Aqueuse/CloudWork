874bf59d6beec75be378f9b6f1daa908124bbf58
Laura Bécognée <jaimaille@gmail.com>
2024-11-17 09:50:57 +0100

    assembling the elements (wordcloud lib + front to back)

--- DataParsing/WordsToCloud.cs
+++ DataParsing/WordsToCloud.cs
@@ -1,20 +1,26 @@
 ﻿using CloudWork.Services;
+using KnowledgePicker.WordCloud;
+using KnowledgePicker.WordCloud.Coloring;
+using KnowledgePicker.WordCloud.Drawing;
+using KnowledgePicker.WordCloud.Layouts;
+using KnowledgePicker.WordCloud.Primitives;
+using KnowledgePicker.WordCloud.Sizers;
+using SkiaSharp;
 
 namespace CloudWork.DataParsing;
 
-public class WordsToCloud {
-    private Source_Offres sourceOffres;
-    
+public class WordsToCloud(Source_Offres sourceOffres) {
     private List<string> words = new ();
     private List<string> descriptions = new ();
+    readonly FranceTravail franceTravail = new ();
 
-    public WordsToCloud(Source_Offres sourceOffres) {
-        this.sourceOffres = sourceOffres;
-    }
-
-    public async Task<bool> GenerateCloud() {
+    public async Task<bool> GenerateCloudFromKeyword(string keyword) {
         if (sourceOffres == Source_Offres.FRANCE_TRAVAIL) {
-            descriptions = FranceTravail.GetWords();
+            descriptions = await franceTravail.GetWordsFromKeywork(keyword);
+
+            if (descriptions.Count == 0) {
+                return false;
+            }
             
             words = CurateWordsListFromDescriptions(descriptions);
         
@@ -31,6 +37,38 @@ public class WordsToCloud {
     }
 
     public async Task DrawCloud(List<string> words) {
+        // get the words frequency on a dictionnary (skip useless french words, etc, see blacklist)
+        
         // effectively draw the cloud
+        var frequencies = new Dictionary<string, int>();
+        
+        // ...collect word frequencies somehow...
+        IEnumerable<WordCloudEntry> wordEntries = frequencies.Select(p => new WordCloudEntry(p.Key, p.Value));
+        
+        var wordCloud = new WordCloudInput(wordEntries) {
+            Width = 1024,
+            Height = 256,
+            MinFontSize = 8,
+            MaxFontSize = 32
+        };
+        
+        var sizer = new LogSizer(wordCloud);
+        using var engine = new SkGraphicEngine(sizer, wordCloud);
+        var layout = new SpiralLayout(wordCloud);
+        var colorizer = new RandomColorizer();
+        var wcg = new WordCloudGenerator<SKBitmap>(wordCloud, engine, layout, colorizer);
+        
+        using var final = new SKBitmap(wordCloud.Width, wordCloud.Height);
+        using var canvas = new SKCanvas(final);
+
+        // Draw on white background.
+        canvas.Clear(SKColors.White);
+        using var bitmap = wcg.Draw();
+        canvas.DrawBitmap(bitmap, 0, 0);
+
+        // Save to PNG
+        using var data = final.Encode(SKEncodedImageFormat.Png, 100);
+        await using var writer = File.Create("cloud.png");
+        data.SaveTo(writer);
     }
 }
--- Services/FranceTravail.cs
+++ Services/FranceTravail.cs
@@ -1,4 +1,6 @@
-﻿namespace CloudWork.Services;
+﻿using System.Net;
+
+namespace CloudWork.Services;
 
 using System.Net.Http.Json;
 using System.Net.Http.Headers;
@@ -9,55 +11,86 @@ using System.Net.Http.Headers;
 public class FranceTravail {
     private readonly HttpClient _httpClient = new();
 
-    public static List<string> GetWords() {
+    private readonly List<string> accessData;
+
+    public FranceTravail() {
+        accessData = ReadAccessData();
+    }
+    
+    private List<string> ReadAccessData() {
+        return File.ReadLines("/ressources/access_FranceTravail.txt").ToList();
+    }
+
+    private void WriteAccessData(List<string> lines) {
+        File.WriteAllLines("/ressources/access_FranceTravail.txt", lines);
+    }
+    
+    public async Task<List<string>> GetWordsFromKeywork(string keyword) {
         List<string> descriptions = new List<string>();
         
-        // try to get data
-        
-        // if code response is 400 or 500
+        // verify validity of the access token
         for (int i = 0; i < 5; i++) {
-            // refresh token
-            // retry to get data with new token
-            // if success, return
+            HttpStatusCode statusCode = await TestAccess(accessData[3]);
+
+            if (statusCode == HttpStatusCode.BadRequest | statusCode == HttpStatusCode.InternalServerError) {
+                // refresh token
+                accessData[3] = await RefreshAccessToken();
+            }
+
+            // if token is still okay, try to get data
+            if (statusCode == HttpStatusCode.OK) {
+                descriptions = await GetDescriptionsFromKeyword(keyword, accessData[3]);
+            }
         }
         
-        // if still failed after 5 attempts
-            // send user to gif page and return
-        return new List<string>();
+        return descriptions;
     }
     
     // get data
-    public async Task<List<string>> GetDescriptionsFromKeyword(string keyword) {
+    private async Task<List<string>> GetDescriptionsFromKeyword(string keyword, string token) {
         List<string> descriptions = new List<string>();
 
+        var request = new HttpRequestMessage {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri("https://api.francetravail.io/partenaire/offresdemploi/v2/offres/search?motsCles="+keyword),
+            Headers =
+            {
+                { "Authorization", "Bearer "+token },
+                { "Accept", "application/json" }
+            }
+        };
+        using var response = await _httpClient.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        
+        var body = await response.Content.ReadAsStringAsync();
+        
+        // how to access descriptions on body ? Serialization ?
+        return descriptions;
+    }
+
+    private async Task<HttpStatusCode> TestAccess(string token) {
         var request = new HttpRequestMessage {
             Method = HttpMethod.Get,
             RequestUri = new Uri("https://api.francetravail.io/partenaire/offresdemploi/v2/offres/search"),
             Headers =
             {
-                { "Authorization", "Bearer jtlqNnfDNHjFYXGtteySufAVrhU" },
-                { "Accept", "application/json" },
-            },
+                { "Authorization", "Bearer "+token },
+                { "Accept", "application/json" }
+            }
         };
         using var response = await _httpClient.SendAsync(request);
-        response.EnsureSuccessStatusCode();
-        var body = await response.Content.ReadAsStringAsync();
-        
-        // will allow us to know if we need to regenerate access token
-        Console.WriteLine(response.StatusCode);
-
-        return descriptions;
+        return response.StatusCode;
     }
     
     // get/refresh access token
-    private async Task<string> GetAccessToken() {
+    private async Task<string> RefreshAccessToken() {
         var client = new HttpClient();
         var request = new HttpRequestMessage(HttpMethod.Post, "https://entreprise.francetravail.fr/connexion/oauth2/access_token?realm=%2Fpartenaire");
         request.Headers.Add("Accept", "application/json;charset=UTF-8");
         var collection = new List<KeyValuePair<string, string>>();
         collection.Add(new("grant_type", "client_credentials"));
-        collection.Add(new("client_id", "PAR_cloudwork_1e00df847d310028f64ea6257f9c9290fbad9ea928da4bf0b346800a7a659733"));
-        collection.Add(new("client_secret", "c6d9880358e27bc2a3267ae1466f8764a03eae8b51d19b373020956b5cb46c41"));
+        collection.Add(new("client_id", accessData[0]));
+        collection.Add(new("client_secret", accessData[1]));
         collection.Add(new("scope", "api_offresdemploiv2 o2dsoffre"));
         var content = new FormUrlEncodedContent(collection);
         request.Content = content;
@@ -65,6 +98,10 @@ public class FranceTravail {
         response.EnsureSuccessStatusCode();
         
         var responseContent = response.Content.ReadAsStringAsync();
+        
+        // TODO : retrieve access token
+        
+        WriteAccessData(new List<string>(){accessData[0], accessData[1], responseContent.Result} );
 
         return "plop";
     }
